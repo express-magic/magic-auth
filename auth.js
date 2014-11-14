54f2a1b804993f0da913aafdf78c24006eeaf23e
Jascha Ehrenreich <jascha@jaeh.at>
2014-11-14 04:16:44 +0100

    made use of next optional

--- auth.js
+++ auth.js
@@ -1,30 +1,29 @@
 'use strict';
 var log     = require('magic-log')
-  , schema  = false
   , passport = require('passport')
   , LocalStrategy = require('passport-local').Strategy
 ;
-exports.init = function(Schema, next) {
-  var User   = Schema.models.User;
-
-  schema = Schema;
+exports.init = function(schema, next) {
+  var User   = schema.models.User;
 
   passport.use(new LocalStrategy({
-      usernameField: 'name',
-      passwordField: 'pass'
-    },
-    function(username, password, done) {
-      User.findOne({ name: username }, function (err, user) {
-        if (err) { return done(err); }
-        if ( ! user || ! user.validatePassword(password) ) {
-          return done(null, false, { message: 'Incorrect username or password.' });
-        }
-        return done(null, user);
-      });
+      usernameField: 'name'
+    , passwordField: 'pass'
     }
-  ));
+    , function(username, password, done) {
+        User.findOne({ name: username }, function (err, user) {
+          if (err) { return done(err); }
+          if ( ! user || ! user.validatePassword(password) ) {
+            return done(null, false, { 
+              message: 'Incorrect username or password.' 
+            });
+          }
+          return done(null, user);
+        });
+      }
+  ) );
 
-  next();
+  if ( typeof next === 'function' ) { next(); }
 }
 
 exports.routes = function (app, req, res, next) {
